adf4f598e208556f97b3ee92f86e2405445deaa2
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-06 02:00:54 +0600

    added game over method

--- Cross The Road/main.py
+++ Cross The Road/main.py
@@ -1,6 +1,7 @@
 from  turtle import Screen
 from player import Player
 from car_manager import Car
+from scoreboard import Score
 import time
 
 player = Player()
@@ -8,14 +9,18 @@ car = Car()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+score = Score()
 
 screen.listen()
 screen.onkeypress(player.move,"Up")
 game_is_on = True
-while game_is_on:ff
+while game_is_on:
     time.sleep(0.1)
     screen.update()
     car.create_car()
     car.move_car()
+    for cars in car.all_cars:
+        if player.distance(cars)<28:
+            score.game_over()
+            game_is_on=False
 screen.exitonclick()
--- Cross The Road/scoreboard.py
+++ Cross The Road/scoreboard.py
@@ -1,7 +1,16 @@
 from turtle import Turtle
 
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 24, "bold")
 
 
-class Scoreboard(Turtle):
-    pass
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.penup()
+        self.ht()
+        self.goto(-280, 280)
+        self.write(f"Level: {self.level}",FONT)
+    def game_over(self):
+        self.goto(0,0)
+        self.write("G.A.M.E   O.V.E.R",FONT)
