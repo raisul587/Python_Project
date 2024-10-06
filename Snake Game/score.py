3ce687dab252fb6a220dc118e1e2ddb15e5d86e7
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-06 21:58:12 +0600

    Keeps track of High Score even if program is closed

--- Snake Game/score.py
+++ Snake Game/score.py
@@ -1,19 +1,25 @@
 from turtle import Turtle
 ALIGNMENT = 'center'
 FONT = ('Minion Pro', 20, 'normal')
+
+
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            h_score = int(data.read())
+            self.high_score = h_score
         self.ht()
         self.penup()
         self.goto(0,270)
         self.color("white")
         self.update_score()
+
     def update_score(self):
         self.clear()
-        self.write(f"score:{self.score} High Score:{self.high_score}" , move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"score:{self.score}      High Score:{self.high_score}" , move=False, align=ALIGNMENT, font=FONT)
         
     def plus_score(self):
         self.score+=1
@@ -22,10 +28,7 @@ class Score(Turtle):
     def reset_score(self):
         if self.score>self.high_score:
             self.high_score=self.score
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
-
-
-    # def game_over(self):
-    #     self.goto(0,0)
-    #     self.write("GAME OVER...." , move=False, align=ALIGNMENT, font=FONT)
