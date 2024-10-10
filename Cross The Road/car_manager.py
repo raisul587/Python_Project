d5f947684ef9419111d8a1c3c9166d5b2c57e4fa
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-10 17:03:13 +0600

    create,move,speeding the car

--- Cross The Road/car_manager.py
+++ Cross The Road/car_manager.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 10 #When Level UP
 class Car:
     def __init__(self):
         self.all_cars = []
