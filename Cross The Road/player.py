c48606d4febdf737a165ff13fbc49d567ae6d01d
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-10 17:03:51 +0600

    create player using turtle

--- Cross The Road/player.py
+++ Cross The Road/player.py
@@ -4,13 +4,12 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-
 class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
-        self.color('black')
         self.penup()
+        self.color('black')
         self.goto_start()
         self.left(90)
 
