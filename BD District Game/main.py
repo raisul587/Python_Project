7b853e89f7c46d589d3e417797bc68d099e91437
raisul587 <70907325+raisul587@users.noreply.github.com>
2024-10-11 01:16:38 +0600

    Learn the Bangladesh Map

--- BD District Game/main.py
+++ BD District Game/main.py
@@ -13,7 +13,7 @@ df = pd.read_csv("Bangladesh_District.csv")
 all_district = df["District"].to_list()
 
 already_guessed = []
-missed_district = []
+
 while len(already_guessed) < 64:
     answer = screen.textinput(f"{len(already_guessed)}/64 Correct", "Guess a District Name")
     if answer is None:  # Check if the user closed the dialog
@@ -21,15 +21,13 @@ while len(already_guessed) < 64:
     answer = answer.title()
 
     if answer == "Exit":
-        for name in all_district:
-            if name not in already_guessed:
-                missed_district.append(name)
+        missed_district = [name for name in all_district if name not in already_guessed  ]
         m_df = pd.DataFrame(missed_district, columns=["Missed Districts"])
         m_df.to_csv("Missed_District.csv", index=False)
         print("The districts you missed are saved in Missed_District.csv. Check it and play again!")
         break  # Exit the loop after saving
 
-    elif answer in all_district and answer not in already_guessed:
+    if answer in all_district and answer not in already_guessed:
         already_guessed.append(answer)
         t = turtle.Turtle()
         t.penup()
